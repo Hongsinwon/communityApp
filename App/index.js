1b0a4dbc8c91b27c900489abf25aa00f0aa39948
Hongsinwon <ghdtlsdnjs12@naver.com>
2022-02-16 18:33:47 +0900

    heroku 배포

--- App/index.js
+++ App/index.js
@@ -4,16 +4,16 @@ const mongoose = require("mongoose");
 const app = express();
 const port = process.env.PORT || 5000;
 
-const config = require("./config/key.js");
+const config = require("./server/config/key.js");
 
 // build파일 불러오기
-app.use(express.static(path.join(__dirname, "../client/build"))); //__dirname => 현재 경로
+app.use(express.static(path.join(__dirname, "./client/build"))); //__dirname => 현재 경로
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/post", require("./Router/post.js"));
-app.use("/api/user", require("./Router/user.js"));
-app.use("/api/reple", require("./Router/reple.js"));
+app.use("/api/post", require("./server/Router/post.js"));
+app.use("/api/user", require("./server/Router/user.js"));
+app.use("/api/reple", require("./server/Router/reple.js"));
 
 app.listen(port, () => {
   mongoose
@@ -28,9 +28,9 @@ app.listen(port, () => {
 });
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
